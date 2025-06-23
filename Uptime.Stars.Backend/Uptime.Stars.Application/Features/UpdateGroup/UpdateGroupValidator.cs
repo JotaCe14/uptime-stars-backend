5cc0c494c76426ffa54335551e33d2dfc269ed88
JotaCe14 <jose.bustamante@dbtarget.pe>
2025-06-23 10:21:16 -0500

    Validator for UpdateGroup

---
+++ Uptime.Stars.Backend/Uptime.Stars.Application/Features/UpdateGroup/UpdateGroupValidator.cs
@@ -0,0 +1,11 @@
+﻿using FluentValidation;
+
+namespace Uptime.Stars.Application.Features.UpdateGroup;
+internal sealed class UpdateGroupValidator : AbstractValidator<UpdateGroupCommand>
+{
+    public UpdateGroupValidator()
+    {
+        RuleFor(command => command.Name)
+            .NotEmpty();
+    }
+}
